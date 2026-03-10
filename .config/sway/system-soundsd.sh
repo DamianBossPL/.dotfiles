5914aefe138f6919de0b592baa9f3c99f73c56ef
DamianBossPL <77542487+DamianBossPL@users.noreply.github.com>
2026-03-10 19:18:16 +0000

    Rename some sway scripts, convert most of execs to systemd user services, use uwsm to stop sway

--- .config/sway/system-soundsd.sh
+++ .config/sway/system-soundsd.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# Udev events without use of root privledges anywhere
-
-# Monitor udev events via script
 udevadm monitor --udev --property | \
 while IFS= read -r line; do
-  # --- CAPTURE EVENT PROPERTIES --- #
+  # Event properties
   if [[ "$line" == ACTION=* ]]; then
     action="${line#ACTION=}"
   elif [[ "$line" == SUBSYSTEM=* ]]; then
@@ -14,14 +11,11 @@ while IFS= read -r line; do
     devtype="${line#DEVTYPE=}"
   fi
 
-  # On empty line (event description ended)
+  # Event description ends with empty line
   if [[ -z "$line" ]]; then
-    # USB
     if [[ "$subsystem" == "usb" && "$devtype" == "usb_device" ]]; then
-      # USB Plug
       if [[ "$action" == "add" ]]; then
         canberra-gtk-play -i device-added &
-      # USB Unplug
       elif [[ "$action" == "remove" ]]; then
         canberra-gtk-play -i device-removed &
       fi
